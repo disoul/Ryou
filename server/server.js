d0a3c93e7f2e12a51aefd0dc0be169cf281a0d46
disoul <starkyds@gmail.com>
2016-10-31 16:14:02 +0800

    server: return html

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@
 var koa = require('koa');
 var router = require('koa-router')();
 var app = new koa();
+var utils = require('../utils');
 
 var pify = require('pify');
 var process = require('process');
@@ -16,15 +17,20 @@ var options = {
   port: process.env.RYOU_PORT || 8008,
 }
 
+const config = await utils.getConfig(); 
 
 router
   .get('/:user/:project', async (ctx, next) => {
-    console.log(next);
+    let user = ctx.params.user;
+    let project = ctx.params.project;
+    let projectPath = path.resolve(config.ryouPath, user, project);
+    ctx.body = await pify(fs.readFile)(path.resolve(projectPath, 'index.html'), {encoding:'utf8'});
   })
 
 app
   .use(router.routes())
   .use(router.allowedMethods());
+
 
 app.listen(options.port);
 console.log('listen on', options.port);
